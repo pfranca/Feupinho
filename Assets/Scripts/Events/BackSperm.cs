18f070b4144fe23f8e2e6b64d33a39897a048609
luis spinola <luismspinola@gmail.com>
2020-03-13 00:54:30 +0000

    Update Menu

---
+++ Assets/Scripts/Events/BackSperm.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackSperm : MonoBehaviour {
+    public float velocityX = 7;
+    void Start() {
+        
+    }
+
+    void Update() {
+        if(transform.position.x < 40) {
+            transform.position += transform.right * (Time.deltaTime * (velocityX));
+        }
+        else {
+            transform.position = new Vector2(-20, transform.position.y); 
+        }
+        
+    }
+}
--- Assets/Scripts/Ui/MenuController.cs
+++ Assets/Scripts/Ui/MenuController.cs
@@ -5,13 +5,56 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MenuController : MonoBehaviour {
     public GameObject loadingPanel;
+    public GameObject playButton;
+    public GameObject optionsButton;
+    public GameObject exitButton;
+    public GameObject backButton;
+    public new Camera camera;
     public Slider slider;
+    public float cameraVelocityX = 40;
+
+
+    bool options = false;
+    bool back = false;
+
+    void Update() {
+        if (options) {
+            if(camera.transform.position.x < 20) {
+                camera.transform.position += transform.right * (Time.deltaTime * (cameraVelocityX * 0.9f));
+            }
+            else {
+                backButton.SetActive(true);
+            }
+        }
+        if(back) {
+            if (camera.transform.position.x >= 0) {
+                camera.transform.position -= transform.right * (Time.deltaTime * (cameraVelocityX * 0.9f));
+            }
+            else {
+                playButton.SetActive(true);
+                optionsButton.SetActive(true);
+                exitButton.SetActive(true);
+            }
+        }
+    }
     public void Play() {
         StartCoroutine(Load(SceneManager.GetActiveScene().buildIndex + 1));
-        
     }
     public void Exit() {
         Application.Quit();
+    }
+    public void Options() {
+        back = false;
+        options = true;
+        playButton.SetActive(false);
+        optionsButton.SetActive(false);
+        exitButton.SetActive(false);
+    }
+    public void Back() {
+        back = true;
+        options = false;
+        backButton.SetActive(false);
+
     }
 
     IEnumerator Load(int index) {
